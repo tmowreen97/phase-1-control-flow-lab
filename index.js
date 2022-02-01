eb453aa413f393e20ebb342077e17bf9e5ce3fb6
Tasreen Mowreen <tmowreen97@gmail.com>
2022-01-31 19:31:55 -0500

    commit tree 55daa73fd449b20d36c81d8a37be4f959326d889

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(howManyFeet){
   // Write your code here!
+  if (howManyFeet<= 400){
+    return ('This one is on me!');
+  } else if (howManyFeet> 2500) {
+    return ('No can do.');
+  } else if (howManyFeet> 2000) {
+    return ('I will gladly take your thirty bucks.');
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city=== 'NYC'? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return ('Thank you so much.');
+    case 'not as generous':
+      return ('Thank you.');
+    default:
+      return ('Bye.');
+  }
 }
